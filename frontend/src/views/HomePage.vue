3f8c3826ce57a83bdc6942d9803d332edeb09d91
0x5e <0x5e@sina.cn>
2026-03-18 12:14:11 +0800

    update homepage

--- frontend/src/views/HomePage.vue
+++ frontend/src/views/HomePage.vue
@@ -1,3 +1,14 @@
 <template>
-  <div />
+  <div>
+    <img class="printer-thumbnail" src="../assets/images/printer_thumbnail_p1s_png.png"/>
+  </div>
 </template>
+
+<script setup lang="ts">
+</script>
+
+<style scoped>
+.printer-thumbnail {
+  height: 50vh;
+}
+</style>
